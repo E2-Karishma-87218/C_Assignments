328b3ee49d26eda3d57a8e1243d18ee0da4539e6
Karishma-Ghadge <karishmaghadge1996@gmail.com>
2024-09-04 15:30:12 +0530

    Assignment 4th Q7 of pointers

--- Fourth_C_assignment/Q7_pointers.c
+++ Fourth_C_assignment/Q7_pointers.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+int n1=20,n2=10;
+void calculate(int,int,int *,int *);
+int main()
+{
+	int calculation,sum,product;
+	calculate(n1,n2,&sum,&product);
 
+	return 0;
+}
+void calculate(int n1,int n2,int *ptr1,int *ptr2)
+{
+	*ptr1=n1+n2;
+	*ptr2=n1*n2;
+	printf("%d + %d = %d\n",n1,n2,*ptr1);
+	printf("%d * %d = %d\n",n1,n2,*ptr2);
+}
