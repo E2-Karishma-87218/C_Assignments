7b445b0c0552d2c509f58db257cc745a2531afbb
Karishma-Ghadge <karishmaghadge1996@gmail.com>
2024-09-04 15:41:14 +0530

    Fourth assigmnet Q7

--- Fourth_C_assignment/Q7_pointers_1.c
+++ Fourth_C_assignment/Q7_pointers_1.c
@@ -1,13 +1,18 @@
 #include<stdio.h>
-int n1=20,n2=10;
 void calculate(int,int,int *,int *);
+
 int main()
 {
+	int n1,n2;
 	int sum,product;
-	calculate(n1,n2,&sum,&product);
 
+	printf("Enter the value of n1 and n2: \n");
+	scanf("%d%d",&n1,&n2);
+
+	calculate(n1,n2,&sum,&product);
 	return 0;
 }
+
 void calculate(int n1,int n2,int *ptr1,int *ptr2)
 {
 	*ptr1=n1+n2;
